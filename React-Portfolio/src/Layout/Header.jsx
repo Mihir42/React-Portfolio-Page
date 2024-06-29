b296589ec6e1068caf9b39c4c7d429ed25abd7d0
Coffee <mihir_mistry@outlook.com>
2024-06-29 01:26:31 +0100

    Navbar added

--- React-Portfolio/src/Layout/Header.jsx
+++ React-Portfolio/src/Layout/Header.jsx
@@ -1,7 +1,19 @@
 import "./Header.scss";
 
 function Header() {
-  return <div className="Header">This is the header</div>;
+  return (
+    <div className="Header">
+      <ul className="List">
+        <li>
+          <a href="">About</a>
+          <a href="">Experience</a>
+          <a href="">Projects</a>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+      Balls
+    </div>
+  );
 }
 
 export default Header;
