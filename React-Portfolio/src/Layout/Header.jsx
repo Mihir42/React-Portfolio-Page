6592cee6410d229d863448707dd8da72ce55707d
Coffee <mihir_mistry@outlook.com>
2024-07-08 00:21:07 +0100

    Added responsive navbar

--- React-Portfolio/src/Layout/Header.jsx
+++ React-Portfolio/src/Layout/Header.jsx
@@ -1,23 +1,32 @@
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IconContext } from "react-icons/lib";
 import "./Header.scss";
 
 function Header() {
+  const [openHamburger, setOpenHamburger] = useState(false);
+
+  const handleHamburger = () => {
+    setOpenHamburger(!openHamburger);
+  };
   return (
-    <div className="Header">
-      <div className="Menu">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <ul className="List">
-        <li>
-          <a href="">About</a>
-          <a href="">Experience</a>
-          <a href="">Projects</a>
-          <a href="">Contact</a>
-        </li>
-      </ul>
-      <div className="Header-Name"> Mihir Mistry</div>
-    </div>
+    <nav className="Header">
+      <p className="Header-Name">Mihir Mistry</p>
+      <a className={openHamburger ? "Nav-Link" : "No-Link"} href="/">
+        About Me
+      </a>
+      <a className={openHamburger ? "Nav-Link" : "No-Link"} href="/">
+        Experience
+      </a>
+      <a className={openHamburger ? "Nav-Link" : "No-Link"} href="/">
+        Projects
+      </a>
+      <a className={openHamburger ? "Nav-Link" : "No-Link"} href="/">
+        Contact
+      </a>
+
+      <RxHamburgerMenu className="Hamburger-Icon" onClick={handleHamburger} />
+    </nav>
   );
 }
 
