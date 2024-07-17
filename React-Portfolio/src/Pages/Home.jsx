444247c17c4d95344d603236ba416c47d57bacc7
Mihir42 <124581857+Mihir42@users.noreply.github.com>
2024-07-17 23:32:50 +0100

    Added flex to home page (#6)

--- React-Portfolio/src/Pages/Home.jsx
+++ React-Portfolio/src/Pages/Home.jsx
@@ -1,21 +1,37 @@
 import Layout from "../Layout/Layout";
-import TextBox from "../UI/TextBox";
-import Luigi from "../assets/Luigi.webp";
+import CuterCat from "../assets/Cat-but-cuter.jpg";
 import "./Home.scss";
 
 function Home() {
   return (
     <Layout>
-      <TextBox title={"Hi, I'm Mihir"}>
-        Full stack developer with knowledge in ReactJS, ExpressJS, JavaScript
-        and Java. Click to find out more.
-      </TextBox>
-      <img className="Profile-Picture" src={Luigi}></img>
-      <button className="Contact-Me" type="button">
-        Contact me
-      </button>
+      <div className="Home-Page-Container">
+        <div className="Container-Left">
+          <div className="Title">
+            Full stack developer with a passion for...
+          </div>
+          <div className="Description">
+            Big company announcment or simple sub-header taking two or more
+            lines.
+          </div>
+          <div className="Buttons-Links">
+            <button className="Projects-Button" type="button">
+              Explore projects
+            </button>
+            <a>Experience</a>
+          </div>
+        </div>
+        <div className="Container-Right">
+          <img className="Profile-Picture" src={CuterCat}></img>
+        </div>
+      </div>
+      <div className="About-Me-Page-Container">
+        <div className="About-Me-Title">About Me</div>
+      </div>
     </Layout>
   );
 }
 
 export default Home;
+
+// https://dev.to/drews256/ridiculously-easy-row-and-column-layouts-with-flexbox-1k01
