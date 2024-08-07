6983cde9f2abecfdbd2aaa63547eb392355a8460
Coffee <mihir_mistry@outlook.com>
2024-08-07 20:49:18 +0100

    I forgot to make a branch again

---
+++ React-Portfolio/src/UI/Caraousel.jsx
@@ -0,0 +1,8 @@
+// Array of objects containing quotes
+// Use a handler that is called when two buttons are pressed
+// It will fetch a object based of index count
+// Count increased when right arrow is selected
+// Count is descresed when left arrow is selected
+// Count must stay between 0 and last index
+// Handler returns the object from array
+// Html is then used to format the quote
