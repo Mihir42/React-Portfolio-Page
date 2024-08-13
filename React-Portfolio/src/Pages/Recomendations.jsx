ae4568f9c61d8c53f12c3a16ba7c6b6c19212f10
Mihir42 <124581857+Mihir42@users.noreply.github.com>
2024-08-13 02:33:00 +0100

    Fixed title, refactored (#16)
    Image styling and card styling

--- React-Portfolio/src/Pages/Recomendations.jsx
+++ React-Portfolio/src/Pages/Recomendations.jsx
@@ -52,7 +52,7 @@ import "./Recomendations.scss";
 const Recomendations = () => {
   return (
     <div className="Recomendations-Container">
-      <h1 className="Recomendations-Title">Recomendations</h1>
+      <h1 className="Recomendations-Title">Recomendations </h1>
       <Caraousel recomendationsList={RecomendationsList}></Caraousel>;
     </div>
   );
