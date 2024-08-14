68dd99d6b33832d289bc21aac18e7db2668f2364
Mihir42 <124581857+Mihir42@users.noreply.github.com>
2024-08-14 01:30:06 +0100

    Completed contact page (#17)
    Added footer but still needs work

--- React-Portfolio/src/Layout/Footer.jsx
+++ React-Portfolio/src/Layout/Footer.jsx
@@ -1,5 +1,12 @@
-function Footer() {
-  return <p></p>;
-}
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="Footer-Container">
+      <h1 className="Footer-Title">This is the footer</h1>
+      <p>2024 © Mihir Mistry All rights reserved.</p>
+    </div>
+  );
+};
 
 export default Footer;
