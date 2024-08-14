d5f43085510ee703bdcd35ec6e959e4572a4dbbf
Coffee <mihir_mistry@outlook.com>
2024-08-14 21:19:43 +0100

    Added Footer
    Forgot to add a branch

--- React-Portfolio/src/Layout/Footer.jsx
+++ React-Portfolio/src/Layout/Footer.jsx
@@ -3,8 +3,58 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="Footer-Container">
-      <h1 className="Footer-Title">This is the footer</h1>
-      <p>2024 © Mihir Mistry All rights reserved.</p>
+      <div className="Footer-Left">
+        <h1 className="Footer-Title">Mihir Mistry</h1>
+        <p className="Footer-Copyright">
+          2024 © Mihir Mistry All rights reserved.
+        </p>
+      </div>
+      <div className="Footer-Right">
+        <div className="Footer-Links">
+          <a className="Footer-Link" href="/">
+            Home
+          </a>
+          <a className="Footer-Link" href="/">
+            About Me
+          </a>
+          <a className="Footer-Link" href="/">
+            Explore my Work
+          </a>
+        </div>
+
+        <div className="Footer-Links">
+          <a className="Footer-Link" href="/">
+            My case studies
+          </a>
+          <a className="Footer-Link" href="/">
+            Weather app
+          </a>
+          <a className="Footer-Link" href="/">
+            Recomendations
+          </a>
+        </div>
+
+        <div className="Footer-Links">
+          <a className="Footer-Link" href="/">
+            Contact
+          </a>
+          <a className="Footer-Link" href="/">
+            Healthcare App
+          </a>
+          <a className="Footer-Link" href="/">
+            Educational Tool
+          </a>
+        </div>
+
+        <div className="Footer-Links">
+          <a className="Footer-Link" href="/">
+            Weather
+          </a>
+          <a className="Footer-Link" href="/">
+            Portfolio Website
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
